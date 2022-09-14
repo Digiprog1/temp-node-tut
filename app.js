7e6244f1f7d00022bd2d3d315a4232b725f87a78
Sunkanmi Adebiyi <103468199+Digiprog1@users.noreply.github.com>
2022-09-14 11:39:27 +0100

    modified these files; 12-http.js, app.js, and created the following files; 12-flatdeep.js, 13-event-emmiter.js, 14-request-event.js, 15-create-big-file.js, 16-streams.js, 17-http-stream.js, 2-async-patterns/2-blocking-code,js, content/big.txt, content/result-mind-grenade.txt

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-// npm - global command, comes with node
-// npm --version
+const { readFile,writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-// local dependency - use it only in this particular Project
-// npm i <packageName>
+    const start = async() => {
+        try {
+            const first = await readFile('./content/first.txt','utf8')
+            const second = await readFile('./content/second.txt', 'utf-8')
+            await writeFile(
+                './content/result-mind-grenade.txt',
+                 `This is AMAZING : ${first} ${second}`,
+                 {flag:'a'}
+                 )
+            console.log(first, second)
+        } catch (error) {
+          console.log(error)
+        }
+    }
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+    start()
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (creates package.json in the root, create properties, etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
 
-const _ = require('lodash')
 
-const items = [1,[2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log('hello world')
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
